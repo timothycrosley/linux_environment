9674b3120acee2cbd2d91413084889dec0353686
Timothy Edmund Crosley <timothy.crosley@gmail.com>
2017-03-03 21:17:18 -0800

    Update suse_install.sh

--- suse_install.sh
+++ suse_install.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 #
-# Installs all packages I commonly use on OpenSuse
-sudo zypper ar -f http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/ vlc
-sudo zypper ar -f http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_13.1/ devel:tools:scm
-sudo zypper ar -f http://download.opensuse.org/repositories/server:/http/openSUSE_Tumbleweed/ http_servers
-sudo zypper ar -f http://download.opensuse.org/repositories/server:/database/openSUSE_Tumbleweed/ database_servers
-sudo zypper ar -f http://download.opensuse.org/repositories/server:/database/openSUSE_Tumbleweed/ database_servers
-sudo zypper ar -f http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
-
+# Install packages that don't require external repositories
+sudo zypper install lsb
 sudo zypper install the_silver_searcher
 sudo zypper install python3-jupyter python3-jupyter_ipython
 sudo zypper install yakuake
 sudo zypper install kate
 sudo zypper install pandoc
-sudo zypper install git-flow
-sudo zypper install google-chrome
-sudo zypper install vlc
 sudo zypper install redis
 sudo zypper install -t pattern devel_python3
-sudo zypper in gcc-c++
+sudo zypper install gcc-c++
 sudo zypper install -t pattern devel_basis
+
+#
+# Install keys
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo rpm --import linux_signing_key.pub
+rm linux_signing_key.pub
+
+#
+# Installs all packages I commonly use on OpenSuse
+sudo zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+sudo zypper ar -f http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/ vlc
+sudo zypper ar -f http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/ games
+sudo zypper ar -f http://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Tumbleweed/ wine
+
+sudo zypper install vlc
+sudo zypper install wine
+sudo zypper install k3b-codecs ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav
+sudo zypper install flash-player
+sudo zypper dup
 
 sudo pip3 install -r ~/environment/requirements.txt -U
 
