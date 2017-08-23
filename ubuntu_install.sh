089fdd0060de9e9a39f66b75d86044399a1b8499
Timothy Edmund Crosley <timothy.crosley@gmail.com>
2017-08-22 23:36:22 -0700

    Create ubuntu_install.sh

---
+++ ubuntu_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install curl
+sudo apt-get install git
+sudo apt-get install yakuake
+sudo apt-get install vim
+sudo apt-get install kate
+sudo apt-get install python3.6
+sudo apt-get install python3.6-dev
+sudo apt-get install silversearcher-ag
+sudo apt-get install pandoc
+sudo apt-get install redis
+sudo apt-get install ubuntu-dev-tools
