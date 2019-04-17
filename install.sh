ef8d2312eed22b6f5eca8cc062888fc668230353
Timothy Crosley <timothy.crosley@gmail.com>
2019-04-17 16:45:25 -0700

    Add licton springs projects to standard linux environment

--- install.sh
+++ install.sh
@@ -17,9 +17,11 @@ mkdir ~/templates
 git clone https://github.com/timothycrosley/python3-project-template ~/templates/python3
 
 mkdir ~/Projects
-mkdir ~/Projects/hugapi
 git clone https://github.com/timothycrosley/isort ~/Projects/isort
 git clone https://github.com/timothycrosley/jiphy ~/Projects/jiphy
+git clone https://github.com/timothycrosley/concentration ~/Projects/concentration
+
+mkdir ~/Projects/hugapi
 git clone https://github.com/hugapi/hug ~/Projects/hugapi/hug
 git clone https://github.com/hugapi/HOPE ~/Projects/hugapi/HOPE
 git clone https://github.com/hugapi/hug_authentication_ldap ~/Projects/hugapi/hug_authentication_ldap
@@ -27,7 +29,8 @@ git clone https://github.com/hugapi/hug_yaml ~/Projects/hugapi/hug_yaml
 git clone https://github.com/hugapi/hug_explainable ~/Projects/hugapi/hug_explainable
 git clone https://github.com/hugapi/hug_peewee ~/Projects/hugapi/hug_peewee
 
-git clone https://github.com/timothycrosley/concentration ~/Projects/concentration
-mkdir ~/documents
+mkdir ~/Projects/licton
+git clone https://github.com/licton/lovelicton ~/Projects/licton/lovelicton
+git clone https://github.com/licton/licton_springs ~/Projects/licton/licton_springs
 
-ln -s ~/environment/isort.cfg ~/.isort.cfg
+ln -s ~/.environment/isort.cfg ~/.isort.cfg
