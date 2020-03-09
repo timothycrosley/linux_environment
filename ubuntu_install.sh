1853980c5d4da9971c263b58814c05f8095f84ac
Timothy Crosley <timothy.crosley@gmail.com>
2020-03-08 23:39:53 -0700

    install all packages without prompting

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade
-sudo apt install curl
-sudo apt install kwrite
-sudo apt install git
-sudo apt install yakuake
-sudo apt install vim
-sudo apt install kate
-sudo apt install retext
-sudo apt install python3
-sudo apt install python3-dev
-sudo apt install python3-venv
-sudo apt install python3-pip
-sudo apt install silversearcher-ag
-sudo apt install pandoc
-sudo apt install redis
-sudo apt install ubuntu-dev-tools
-sudo apt install libu2f-udev
+sudo apt install -y curl
+sudo apt install -y kwrite
+sudo apt install -y git
+sudo apt install -y yakuake
+sudo apt install -y vim
+sudo apt install -y kate
+sudo apt install -y retext
+sudo apt install -y python3
+sudo apt install -y python3-dev
+sudo apt install -y python3-venv
+sudo apt install -y python3-pip
+sudo apt install -y silversearcher-ag
+sudo apt install -y pandoc
+sudo apt install -y redis
+sudo apt install -y ubuntu-dev-tools
+sudo apt install -y libu2f-udev
 
-sudo apt install redshift
-sudo apt install plasma-applet-redshift-control
+sudo apt install -y redshift
+sudo apt install -y plasma-applet-redshift-control
 
-sudo snap install bitwarden
-sudo snap install bw
+sudo snap install -y bitwarden
+sudo snap install -y bw
 
 sudo python3 -m pip install -U concentration isort hug allow
 
-sudo apt install kubuntu-desktop
+sudo apt install -y kubuntu-desktop
 sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg vlc
 sudo dpkg-reconfigure libdvd-pkg
 
 sudo add-apt-repository ppa:wereturtle/ppa
 sudo apt-get update
-sudo apt-get install ghostwriter
+sudo apt-get -y install ghostwriter
